252705dfc71013782a51b92787fbe1bc5a1cc5c4
NaniLevi <nani.levi@yandex.ru>
2017-02-01 17:32:28 +0300

    sessons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
     unless params[:session].blank?
-       user = User.find_by(email: params[:session][:email].downcase)
-       if user && user.find_by(params[:session][:password])
+      user = User.find_by(email: params[:session][:email].downcase)
         # Sign the user in and redirect to the user's show page.
         sign_in user
         redirect_to work_url  # goto work place
         # redirect_to user  # previous
-
       else
         # Create an error message and re-render the signin form.
         flash.now[:alert] = 'Login failed. Invalid email/password combination. Repeat'
         # Not quite right!
-        render 'new'
+      render 'new'
       end
     end
 
@@ -27,4 +24,4 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_url
   end
-end
+
